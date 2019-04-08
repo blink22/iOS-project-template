d000f4c93b878c156d773275f6e446295858db16
Ahmed Abdelmagied <ahmed.abdelmaged@blink22.com>
2019-04-08 15:42:11 +0200

    Cookiecutter intialization

---
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{ cookiecutter.app_name }}
+//
+//  Created by Ahmed Abdelmagied on 4/8/19.
+//  Copyright © 2019 Blink22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
