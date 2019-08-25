8ef4c7efd5a0306977499254e5f98f93bf890bce
Ahmed Abdelmagied <ahmed.abdelmaged@blink22.com>
2019-08-25 20:09:16 +0200

    Add git repository initialization

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,7 +18,19 @@ def install_cocoapods():
     os.system("pod install")
 
 
+def git_init():
+    go_to_project_dir()
+    os.system("git init >/dev/null")
+    os.system("git add -A . >/dev/null")
+    os.system("git commit -m \"Initial Commit\" >/dev/null")
+    os.system("xed . >/dev/null")
+
+
 if __name__ == "__main__":
     print("> Installing Cocoapods")
     install_cocoapods()
+
+    print("> Initializing git repository")
+    git_init()
+
     exit(0)
