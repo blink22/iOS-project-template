2c8dfb5351b0d990542e7b20e3800d326075e2a7
Omar Salah Elboredy <omarfoe94@gmail.com>
2019-09-10 20:34:08 +0200

    Change code structure to VIPER

---
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/Modules/Home/Views/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  {{ cookiecutter.app_name }}
+//
+//  Created by {{ cookiecutter.author }} on {% now 'local', '%d %b %Y' %}.
+//  Copyright © {% now 'local', '%Y' %} {{ cookiecutter.organization_name }}. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
